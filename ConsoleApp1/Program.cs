0f709b741488e241e3cc9953c617b9144fa1618e
Aya Abu <someone990@yahoo.com>
2019-06-25 00:27:46 -0700

    creat the main code for a calculator using c#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using ConsoleApp1.classes;
+
+namespace ConsoleApp1.classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+
+           
+            string[] signs = new string[4] { "+", "-", "*", "/" };
+            double num1, num2;
+
+            Console.WriteLine("My Calculator!");
+
+            Console.WriteLine("Enter your first number: ");
+            string n1 = Console.ReadLine();
+            num1 = calc.NoErr(n1);
+
+
+            Console.WriteLine("Enter your Second number: ");
+            string n2 = Console.ReadLine();
+            num2 = calc.NoErr(n2);
+
+         
+
+            Console.WriteLine("Enter the sign of your operator: ");
+            string inputSign = Console.ReadLine();
+            inputSign = calc.validOperations(inputSign, signs);
+            calc.Calculate(num1, num2, inputSign,signs);
+
+            
+
+            Console.WriteLine("Write Yes if you want to restart your application.");
+
+            string yesNo = Console.ReadLine();
+            if (yesNo == "yes")
+            {
+
+            }
+
+
+        }
+    }
+}
+
---
+++ ConsoleApp1/classes/calc.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1.classes
+{
+    class calc
+    {
+        
+        public string[] operations = { "+", "-", "/", "*" };
+
+
+
+        public static double Add(double a, double b)
+        {
+           double result = a + b;
+            return result;
+        }
+
+        public static double Sub(double a, double b)
+        {
+            double result = a - b;
+            return result;
+        }
+
+        public static double Mult(double a, double b)
+        {
+            double result = a * b;
+            return result;
+
+        }
+
+        public static double Divide(double a, double b)
+        {
+            double result = a / b;
+            return result;
+        }
+
+        public static double NoErr(string s)
+        {
+            bool noMistakes = Double.TryParse(s, out double num);
+            while (!noMistakes)
+            {
+                Console.Write("Please Enter a valid number.");
+                noMistakes = Double.TryParse(Console.ReadLine(), out num);
+            }
+            return num;
+
+        }
+
+
+        public static string validOperations(string s , string[] arr)
+        {
+            string[] operations = { "+", "-", "/", "*" };
+            while (!operations.Contains(s))
+            {
+                Console.WriteLine("Please enter a valid operation");
+                s = Console.ReadLine();
+            }
+            return s;
+            
+
+        }
+
+        public static void Calculate(double num1, double num2, string inputSign, string[] signs)
+        {
+            double result;
+            if (inputSign == signs[0])
+            {
+                result = Add(num1, num2);
+                Console.WriteLine("The Addition of your two numbers is: " + result);
+            }
+
+            else if (inputSign == signs[1])
+            {
+                result = calc.Sub(num1, num2);
+                Console.WriteLine("The Subtraction of your two numbers is: " + result);
+            }
+
+            else if (inputSign == signs[2])
+            {
+                result = calc.Mult(num1, num2);
+                Console.WriteLine("The multiplication of your two numbers is: " + result);
+            }
+
+            else if (inputSign == signs[3])
+            {
+                result = calc.Divide(num1, num2);
+                Console.WriteLine("The division of your two numbers is: " + result);
+            }
+            
+        }
+        
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
